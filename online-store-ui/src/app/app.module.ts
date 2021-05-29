3677b9029d585e17acf4a335c58191b3cf088341
Edvinas <edvinasalimas98@gmail.com>
2021-05-29 21:54:41 +0300

    starting to get data from server

--- online-store-ui/src/app/app.module.ts
+++ online-store-ui/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ProductListComponent} from "./products/product-list.component";
 import {FormsModule} from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -13,10 +13,9 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- online-store-ui/src/app/products/product.service.ts
+++ online-store-ui/src/app/products/product.service.ts
@@ -1,10 +1,19 @@
 import {IProduct} from "./product";
 import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productUrl = "localhost:8080/products"
+  constructor(private http: HttpClient) {
+  }
+
+  // getProducts(): Observable<IProduct[]> {
+  //   return this.http.get<IProduct[]>(this.productUrl);
+  // }
 
   getProducts(): IProduct[] {
     return [
