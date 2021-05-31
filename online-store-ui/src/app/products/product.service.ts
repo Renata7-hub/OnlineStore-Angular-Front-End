e2a52082814176c28ab5a9f428bce7d137394546
Edvinas <edvinasalimas98@gmail.com>
2021-05-31 18:36:04 +0300

    working

--- online-store-ui/src/app/products/product.service.ts
+++ online-store-ui/src/app/products/product.service.ts
@@ -8,7 +8,7 @@ import {catchError, tap} from "rxjs/operators";
   providedIn: 'root'
 })
 export class ProductService {
-  private productUrl = "http://localhost:8080/products/get-all"
+  private productUrl = "http://localhost:8080/product/get-all"
   constructor(private http: HttpClient) {
   }
 
