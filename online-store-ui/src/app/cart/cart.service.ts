a9f1785e0a8f0817aeb22aefae019ce4dbc7d942
Edvinas <edvinasalimas98@gmail.com>
2021-06-19 10:05:58 +0300

    made product in cart update simpler

--- online-store-ui/src/app/cart/cart.service.ts
+++ online-store-ui/src/app/cart/cart.service.ts
@@ -17,6 +17,7 @@ export class CartService {
   private removeFromCartProductUrl = 'http://localhost:8080/cart/delete/';
   private addQuantityToProductInCartUrl = 'http://localhost:8080/cart/add-quantity/';
   private subtractQuantityToProductInCartUrl = 'http://localhost:8080/cart/subtract-quantity/';
+  private changeProductQuantityInCartUrl = 'http://localhost:8080/cart';
 
   constructor(private http: HttpClient) { }
 
@@ -52,11 +53,27 @@ export class CartService {
   }
 
   addQuantityToProduct(cart: Cart) {
-    return this.http.post<Cart>(this.addQuantityToProductInCartUrl, cart);
+    var updateCart = {
+      id: cart.id,
+      product: cart.product,
+      quantity: 1
+    };
+    console.log(updateCart);
+    return this.http.put<Cart>(this.changeProductQuantityInCartUrl, updateCart).pipe(
+      tap(data => console.log("All", JSON.stringify(data)))
+    );
   }
 
   subtractQuantityToProduct(cart: Cart) {
-    return this.http.post<Cart>(this.subtractQuantityToProductInCartUrl, cart);
+    var updateCart = {
+      id: cart.id,
+      product: cart.product,
+      quantity: -1
+    };
+    console.log(updateCart);
+    return this.http.put<Cart>(this.changeProductQuantityInCartUrl, updateCart).pipe(
+      tap(data => console.log("All", JSON.stringify(data)))
+    );
   }
 
   getTotalPrice(): Observable<number> {
