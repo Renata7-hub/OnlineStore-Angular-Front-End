4026dc77ab11f7e3329be16e1dbd1eab17947003
Edvinas <edvinasalimas98@gmail.com>
2021-07-10 22:40:15 +0300

    Added guards to admin only pages

---
+++ online-store-ui/src/app/users/users-guard.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersGuardGuard implements CanActivate {
+
+  constructor(private router: Router) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (sessionStorage.getItem('role') === 'ADMIN') {
+      return true;
+    } else{
+      alert('You do not have the permission');
+      this.router.navigate(['/welcome']);
+      return false;
+    }
+  }
+
+}
--- online-store-ui/src/app/users/users.component.ts
+++ online-store-ui/src/app/users/users.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginService} from "../login/login.service";
 import {UserInterface} from "../login/user.interface";
-import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
+import {MatDialog} from "@angular/material/dialog";
+import {RegisterComponent} from "../register/register.component";
+import {RegisterAdminComponent} from "../register-admin/register-admin.component";
 
 @Component({
   selector: 'app-users',
@@ -14,11 +17,33 @@ export class UsersComponent implements OnInit {
   displayedColumns: string[] = ['userId', 'userName', 'password', 'first-name', 'last-name','age', 'email', 'role'];
   users: UserInterface[] = [];
   p: number = 1;
+  role!: string | null;
+  firstName!: string;
+  lastName!: string;
+  age!: number;
+  email!: string;
+  userName!: string;
+  password!: string;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService,
+              public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.loginService.getAllUsers().subscribe(data => this.users = data);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(RegisterAdminComponent, {
+      width: '400px',
+      data: { userName: this.userName,
+        password: this.password,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        age: this.age,
+        role: this.role
+      }
+    });
   }
 
 }
